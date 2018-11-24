a055ff2409ea09b5907387d8ca8f9b26c0251271
ltenore <leightenore@gmail.com>
2018-11-24 13:46:26 -0700

    updated HTML and CSS files

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//Global Variables
+
+var characters = ["harry", "hermione", "dumbledore", "voldemort"];
+
+var harry = {
+    name: "Harry";
+    pointValue: ;
+};
+
+var hermione = {
+    name: "Hermione";
+    pointValue: ;
+};
+
+var dumbledore = {
+    name: "Dumbledore";
+    pointValue: ;
+};
+
+var voldemort = {
+    name: "Voldemort";
+    pointValue: ;
+};
+
+//Fuctions
+
+//Main Process
